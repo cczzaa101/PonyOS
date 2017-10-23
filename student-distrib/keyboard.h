f0f0dba8f6661946b6b4f8d41fc944f92868bbd0
cczzaa101 <ced.amspa@hotmail.com>
2017-10-23 16:46:38 -0500

    comments for keyboard

--- student-distrib/keyboard.h
+++ student-distrib/keyboard.h
@@ -2,5 +2,7 @@
 #include "i8259.h"
 #include "tests.h"
 #include "scancode.h"
+/*keyboard initialization*/
 extern void keyboard_init();
+/*handle keyboard interrupt*/
 extern void keyboard_interrupt_handler();
