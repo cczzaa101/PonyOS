9e9042b600ad62f9c7d8ac9a42d27da4b36430dd
cczzaa101 <ced.amspa@hotmail.com>
2017-10-22 20:08:59 -0500

    handle keyboard interrupt

---
+++ student-distrib/keyboard.c
@@ -0,0 +1,25 @@
+#include "lib.h"
+#include "i8259.h"
+#define KEYBOARD_DATA_PORT 0x60
+#define KEYBOARD_STATUS_PORT 0x64
+#define KEYBOARD_COMMAND_PORT 0x64
+#define KEYBOARD_IRQ_NUM 1
+
+void keyboard_init()
+{
+    /* keyboard irq locate at 1*/
+    enable_irq(KEYBOARD_IRQ_NUM);
+}
+
+void keyboard_interrupt_handler()
+{
+    cli(); //disable interrupts
+    unsigned char keyboard_buffer;
+    while( (inb(KEYBOARD_STATUS_PORT)&0x1)>0 ) //the buffer is still full
+    {
+        keyboard_buffer = inb(KEYBOARD_DATA_PORT);
+        printf(keyboard_buffer);
+    }
+    send_eoi(KEYBOARD_IRQ_NUM);
+    sti(); //re-enable interrupts
+}
---
+++ student-distrib/keyboard.h
@@ -0,0 +1,5 @@
+#include "lib.h"
+#include "i8259.h"
+
+export void keyboard_init();
+export void keyboard_interrupt_handler();
