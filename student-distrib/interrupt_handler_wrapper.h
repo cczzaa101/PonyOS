cea30fce3149159bd19b19445588fd9e03a582ff
cczzaa101 <ced.amspa@hotmail.com>
2017-10-23 16:42:20 -0500

    comment for interrupt handler

--- student-distrib/interrupt_handler_wrapper.h
+++ student-distrib/interrupt_handler_wrapper.h
@@ -3,9 +3,11 @@
 #define IHW
 #ifndef ASM
 #include "x86_desc.h"
-
+/* assembly wrapper for keyboard interrupt handler */
 extern void keyboard_handler_wrapper();
+/* assembly wrapper for rtc interrupt handler */
 extern void rtc_handler_wrapper(); 
+/* assembly wrapper for general interrupt handler */
 extern void general_handler_wrapper(); 
 #endif
 #endif
